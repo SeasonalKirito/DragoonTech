be54fde00059c3f2e718e6dc1479a6058f8e1710
Proxy <99521865+SeasonalKirito@users.noreply.github.com>
2023-01-24 02:18:55 -0700

    Update Games.lua

--- DragoonTech/Games.lua
+++ DragoonTech/Games.lua
@@ -1,1 +1,18 @@
+repeat
+    wait()
+until game:IsLoaded()
+
+
+
+
+
+local Games = {
+    [] = "",
+}
+
+if Games[game.PlaceId] then
+    loadstring(game:HttpGet(Games[game.PlaceId]))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SeasonalKirito/DragoonTech/main/DragoonTech/Universal.lua"))()
+end
 
